0bfcf41c0bb20df6e1d850bca1038a8122d29d74
Sefrion <ilyenkov96@gmail.com>
2025-01-25 09:19:23 +0200

    made change to environment variables

--- src/lib/axiosClient.ts
+++ src/lib/axiosClient.ts
@@ -3,8 +3,8 @@ import axios from 'axios';
 export const axiosClient = axios.create({
 	baseURL: 'https://free-to-play-games-database.p.rapidapi.com/api',
 	headers: {
-		'x-rapidapi-key': import.meta.env.RAPIDAPI_KEY as string,
-		'x-rapidapi-host': import.meta.env.RAPIDAPI_HOST as string
+		'x-rapidapi-key': import.meta.env.VITE_RAPIDAPI_KEY as string,
+		'x-rapidapi-host': import.meta.env.VITE_RAPIDAPI_HOST as string
 	}
 });
 
